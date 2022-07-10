80f3f5643ea31147515e0333bbf0ff1be6c6630b
sss2sssss <soh.lin.siong@doctoroncall.com.my>
2022-07-10 16:09:10 +0800

    async await

--- db-query/dbFunctionDebtTransaction.js
+++ db-query/dbFunctionDebtTransaction.js
@@ -1,19 +1,17 @@
 let dbQuery = require("./dbQuery");
-let dbUser = require("./dbFunctionUser");
-let dbTransactionLog = require("./dbFunctionTransactionLog");
 let timeUtil = require("../utils/time");
 
 const createTableString = 'CREATE TABLE IF NOT EXISTS debtTransaction(id INTEGER PRIMARY KEY, performUserId INTEGER NOT NULL, toUserId INTEGER NOT NULL, debtAmount TEXT NOT NULL, lastUpdateTimestamp TEXT NOT NULL)';
 
 function getParticularDebtTransaction(performUserId, toUserId)
 {
-    return new Promise(function(resolve, reject){
+    return new Promise(async function(resolve, reject){
         let sql = `SELECT id, debtAmount
         FROM debtTransaction
         WHERE performUserId  = ? AND toUserId = ?`;
 
-        dbQuery.createOrGetDbConnection()
-        .then((db) => {
+        try {
+            let db = await dbQuery.createOrGetDbConnection()
             db.serialize(() => {
                 db.run(createTableString)
                     .get(sql, [performUserId, toUserId], (err, row) => {
@@ -40,21 +38,20 @@ function getParticularDebtTransaction(performUserId, toUserId)
                         }
                     });
               });
-        })
-        .catch((err) => {
+        } catch (err) {
             reject(err);
-        });
+        }
     });
 }
 
 function createDebtTransaction(performUserId, toUserId, debtAmount)
 {
-    return new Promise(function(resolve, reject){
+    return new Promise(async function(resolve, reject){
         let sql = `INSERT INTO debtTransaction (performUserId, toUserId, debtAmount, lastUpdateTimestamp)
         VALUES (?, ?, ?, ?)`;
 
-        dbQuery.createOrGetDbConnection()
-        .then((db) => {
+        try {
+            let db = await dbQuery.createOrGetDbConnection()
             db.serialize(() => {
                 db.run(createTableString)
                     .run(sql, [performUserId, toUserId, debtAmount, timeUtil.getCurrentTime()], (err) => {
@@ -69,23 +66,22 @@ function createDebtTransaction(performUserId, toUserId, debtAmount)
                         }
                     });
               });
-        })
-        .catch((err) => {
+        } catch (err) {
             reject(err);
-        });
+        }
     });
 }
 
 function updateDebtTransaction(transactionId, performUserId, toUserId, debtAmount)
 {
-    return new Promise(function(resolve, reject){
+    return new Promise(async function(resolve, reject){
         let sql = `DELETE FROM debtTransaction where id = ?`;
 
-        dbQuery.createOrGetDbConnection()
-        .then((db) => {
+        try {
+            let db = await dbQuery.createOrGetDbConnection()
             db.serialize(() => {
                 db.run(createTableString)
-                    .run(sql, transactionId, (err) => {
+                    .run(sql, transactionId, async (err) => {
                         if (err) {
                             db.close();
                             reject(err.message);
@@ -93,32 +89,30 @@ function updateDebtTransaction(transactionId, performUserId, toUserId, debtAmoun
                         else
                         {
                             db.close();
-                            createDebtTransaction(performUserId, toUserId, debtAmount)
-                            .then(() => {
+                            try {
+                                await createDebtTransaction(performUserId, toUserId, debtAmount)
                                 resolve(true);
-                            })
-                            .catch((err) => {
+                            } catch (err) {
                                 reject(err);
-                            });
+                            }
                         }
                     });
               });
-        })
-        .catch((err) => {
+        } catch (err) {
             reject(err);
-        });
+        }
     });
 }
 
 function getOwnToTransaction(userId)
 {
-    return new Promise(function(resolve, reject){
+    return new Promise(async function(resolve, reject){
         let sql = `SELECT id, toUserId, debtAmount
         FROM debtTransaction
         WHERE performUserId  = ? AND (debtAmount != '0.00' AND debtAmount != '0')`;
 
-        dbQuery.createOrGetDbConnection()
-        .then((db) => {
+        try {
+            let db = await dbQuery.createOrGetDbConnection()
             db.serialize(() => {
                 db.run(createTableString)
                     .all(sql, [userId], (err, row) => {
@@ -133,22 +127,21 @@ function getOwnToTransaction(userId)
                         }
                     });
               });
-        })
-        .catch((err) => {
+        } catch (err) {
             reject(err);
-        });
+        }
     });
 }
 
 function getOwnFromTransaction(userId)
 {
-    return new Promise(function(resolve, reject){
+    return new Promise(async function(resolve, reject){
         let sql = `SELECT id, performUserId, debtAmount
         FROM debtTransaction
         WHERE toUserId  = ? AND (debtAmount != '0.00' AND debtAmount != '0')`;
 
-        dbQuery.createOrGetDbConnection()
-        .then((db) => {
+        try {
+            let db = await dbQuery.createOrGetDbConnection()
             db.serialize(() => {
                 db.run(createTableString)
                     .all(sql, [userId], (err, row) => {
@@ -163,10 +156,9 @@ function getOwnFromTransaction(userId)
                         }
                     });
               });
-        })
-        .catch((err) => {
+        } catch (err) {
             reject(err);
-        });
+        }
     });
 }
 
